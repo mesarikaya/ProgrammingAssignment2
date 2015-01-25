755b19f733ebe5c403d7e06737b2c2f34acf2852
mesarikaya <mesarikaya@users.noreply.github.com>
2015-01-25 02:18:31 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## [Creates cache in order to retrieve the later when needed]
+## Same methodology with the vector example
+## 1. Set the matrix 2. Get the matrix 3. Set the inverse 4. Get the inverse 
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list( set=set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
+        
+        
+        
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+        ## Call the get method to see if there is an earlier calculation
+        m <- x$getinv()
+        if(!is.null(m)) {
+                
+                if (identical(x$set(y),x$get())==TRUE){
+                ## If there is earlier calculation or same data set it provides a below message and returns already claculated number
+                   message("getting cached data")
+                   return(m)
+                }
+        }
+        ## if there is no earlier calculation, the code needs to solve the inverse and set it for future use
+        y <-x$get() ## get the y for setting the old matrix
+        x$set(y)   ## set the old matrix so that we can compare later if the new matrix is the same
+        m <- solve(y, ...)
+        x$setinv(m)
+        m
 }
